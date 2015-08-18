263b94be827b942927ed5ecdb1ecaa1d759851a5
Salvatore Niro <salvatore.niro@outlook.com>
2015-08-17 22:42:05 -0600

    init

---
+++ README.md
@@ -0,0 +1,80 @@
+﻿# ng-accordian
+A simple, dependency free AngularJS accordian style directive
+
+# Getting Started
+
+ * Include the `ngAccordian` module as a dependency in your AngularJS application 
+
+```javascript
+angular.module('app', ['ngAccordian'])
+```
+
+ * Include the `ngAccordian.css` style sheet
+
+```html
+<link rel="stylesheet" href="/ngAccordian.css" />
+```
+
+# Basic Usage
+
+**With templating** via `content-url`
+```html
+<accordian >
+  <toggle content-url="/some-content.html"></toggle>
+  <toggle content-url="/some-content.html"></toggle>
+  <toggle content-url="/some-content.html"></toggle>
+</accordian>
+```
+
+**With configuration**
+```html
+<accordian>
+  <toggle ng-repeat="c in content" content-url="{{ c.value }}"></toggle>
+</accordian>
+```
+
+```javascript
+$scope.content = [
+  { 'value': '/views/demo-content.html' },
+  { 'value': '/views/demo-content.html' },
+  { 'value': '/views/demo-content.html' }
+];
+```
+
+**With explicit content** via `content`
+
+```html
+<accordian>
+  <toggle ng-repeat="c in content" content="{{ c.value }}"></toggle>
+</accordian>
+```
+
+```javascript
+$scope.content = [
+  { 'value': '<span> er </span>' },
+  { 'value': '<span> um </span>' },
+  { 'value': '<span> eh </span>' }
+];
+```
+
+**As a standalone dropdown**
+
+```html
+<toggle content-url="/views/demo-content.html"></toggle>
+```
+
+# Attribute Usage
+
+| Attribute							| Description																							| Example																					|
+| ----------------------------------|:------------------------------------------------------------------------------------------------------|:------------------------------------------------------------------------------------------|
+|  `content-url`					| Specifies the location to <br>the HTML template														| `<toggle content-url="/views/demo-content.html"></toggle>`								|
+|  `content`						| Specifies the content markup																			| `<toggle content="{{ content }}></toggle>`<br> `$scope.content = '<span>stuff</span>'`    |
+|  `close-others`					| Specifies whether to display<br> one pane at a time													| `<accordian close-others="true | false">`													|
+|  `toggle-icon`<br> (optional)		| Specifies a built in icon <br><br> may be applied to <br> `<accordian>` or `<toggle>`					| `<accordian toggle-icon="chevron | plus">` <br> `<toggle toggle-icon="chevron | plus">`	|
+
+# Toggle Icons
+
+| Value                     | Demo												| 
+| ------------------------- |:--------------------------------------------------|
+|  `toggle-icon="chevron"`  | [JSFiddle](http://jsfiddle.net/sniro/1eovLxpo/)	|
+|  `toggle-icon="plus"`     | [JSFiddle](http://jsfiddle.net/sniro/hvczkjeo/)	|     
---
+++ ngAccordian.js
@@ -0,0 +1,128 @@
+﻿angular.module('ngAccordian', [])
+.factory('accordianStyleFactory', [function () {
+
+    (function () {
+        var css = '.plus div { width: 50%; height: 50%; float: left; box-sizing: border-box; } .chevron { position: relative; box-sizing: border-box; }',
+            head = document.head || document.getElementsByTagName('head')[0],
+            style = document.createElement('style');
+
+        style.type = 'text/css';
+        if (style.styleSheet) {
+            style.styleSheet.cssText = css;
+        } else {
+            style.appendChild(document.createTextNode(css));
+        }
+
+        head.appendChild(style);
+    }());
+
+    function getStyle(code) {
+        switch (code) {
+            case 'chevron': this.style = '<div class="chevron" ng-style="{\'height\':  height  + \'px\', \'width\':  height + \'px\'}" ng-class="{\'open\': toggle}"><div class="chevron" ng-style="{\'height\':  (height / 2) + \'px\', \'width\':  (height / 2) + \'px\', \'left\':  (height / 4) + \'px\', \'top\':  (height / 4) + \'px\'}"></div></div>';
+                break;
+            case 'plus':
+                this.style = '<div class="plus" ng-style="{\'height\':  ((height * 0.67) | number: 0 ) + \'px\', \'width\':  ((height * 0.67) | number: 0 ) + \'px\', \'padding\': (height / 6) + \'px\'}" ng-class="{\'open\': toggle}"><div></div><div></div><div></div><div></div></div>';
+                break;
+            default:
+                this.style = '';
+                break;
+        }
+
+        return this.style;
+    };
+
+    return {
+        getStyle: getStyle
+    }
+}])
+.directive('accordian', ['accordianStyleFactory', function (accordianStyleFactory) {
+    return {
+        scope: {
+            closeOthers: '@',
+            toggleIcon: '@'
+        },
+        controller: function ($scope) {
+            this.getConfiguration = function () {
+                return {
+                    'closeOthers': JSON.parse($scope.closeOthers || false) || false,
+                    'toggleIcon': accordianStyleFactory.getStyle($scope.toggleIcon || 0)
+                }
+            }
+        }
+    }
+}])
+.directive('toggle', ['$compile', 'accordianStyleFactory', '$timeout', function ($compile, accordianStyleFactory, $timeout) {
+    return {
+        scope: {
+            content: '@',
+            contentUrl: '@',
+            toggleIcon: '@'
+        },
+        require: '?^accordian',
+        link: function (scope, elem, attrs, parent) {
+
+            console.log(scope.contentUrl);
+
+            var config = parent ? parent.getConfiguration() : {};
+
+            elem.css('display', 'block');
+
+            var style = scope.toggleIcon ? accordianStyleFactory.getStyle(scope.toggleIcon) : parent ? config.toggleIcon : '';
+
+            var content = scope.contentUrl ? '<div style="overflow: hidden" ng-include="contentUrl" class="toggle-body"></div>' : '<div style="overflow: hidden" ng-html="content" class="toggle-body"></div>';
+
+            var tpl =
+                '<div class="toggle-header" ng-click="toggleBody()">' +
+                    style +
+                '</div>' +
+                '</div>' +
+                    '<div ng-class="{\'open\': toggle}">' +
+                    content +
+                '</div>';
+
+            elem.append(angular.element(tpl));
+
+            $compile(elem.contents())(scope);
+
+            scope.height = elem[0].firstChild.clientHeight;
+
+            scope.toggleBody = function () {
+                if (!scope.toggle && (config.closeOthers)) {
+                    scope.$parent.$broadcast('close', scope.toggle);
+                }
+
+                scope.toggle = scope.toggle ? false : true;
+            }
+
+            scope.$on('close', function (event, toggle) {
+                scope.toggle = toggle;
+            });
+        }
+    }
+}])
+.directive('toggleBody', [function () {
+    return {
+        restrict: 'C',
+        link: function (scope, elem, attrs) {
+
+            scope.$watch('toggle', function (n, o) {
+                var css = n ? { 'max-height': elem[0].scrollHeight + 'px', 'transition': 'all .2s ease-out' } : { 'max-height': 0, 'transition': 'all .2s ease-out' }
+                elem.css(css);
+            });
+        }
+    }
+}])
+.directive('ngHtml', ['$compile', function ($compile) {
+    return function (scope, elem, attrs) {
+        if (attrs.ngHtml) {
+            elem.html(scope.$eval(attrs.ngHtml));
+            $compile(elem.contents())(scope);
+        }
+        scope.$watch(attrs.ngHtml, function (newValue, oldValue) {
+            if (newValue && newValue !== oldValue) {
+                elem.html(newValue);
+                $compile(elem.contents())(scope);
+            }
+        });
+    };
+}]);
