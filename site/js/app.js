f1c7c5322d8bc8686b7c768de5ad799f0d05e29b
Salvatore Niro <salvatore.niro@outlook.com>
2015-08-21 14:10:37 -0600

    adding site to repo

---
+++ site/js/app.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('app', ['ngAccordian']);
+
+app.controller('ctrl', function ($scope) {
+    $scope.content = [
+        { 'value': '/views/demo-content.html' },
+        { 'value': '/views/demo-content.html' },
+        { 'value': '/views/demo-content.html' }
+    ];
+
+    $scope.content2 = [
+        { 'value': '<span>yo!</span>' },
+        { 'value': '<span>yo!</span>' },
+        { 'value': '<span>yo!</span>' }
+    ];
+});
+
