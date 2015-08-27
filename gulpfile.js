3ec22c43e64457fb9991f3051b4c121ce736c266
Salvatore Niro <salvatore.niro@outlook.com>
2015-08-27 14:26:45 -0600

    dist; adding gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 ﻿var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
-gulp.task('hello-world', function() {
-    console.log('inittttttt');
+gulp.task('compress', function () {
+	return gulp.src('src/js/*.js')
+	  .pipe(uglify())
+	  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass', function () {
+	gulp.src('src/sass/*.scss')
+	  .pipe(sass().on('error', sass.logError))
+	  .pipe(gulp.dest('src/css'));
 });
