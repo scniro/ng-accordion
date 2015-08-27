8e1863c3aaedaab8aa8c7ef75771e07b7abcdfe6
Salvatore Niro <salvatore.niro@outlook.com>
2015-08-27 17:26:13 -0600

    adding sass & prefixer gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
-﻿var gulp = require('gulp');
+﻿/// <binding ProjectOpened='sass:watch' />
+var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('compress', function () {
 	return gulp.src('src/js/*.js')
-	  .pipe(uglify())
-	  .pipe(gulp.dest('dist'));
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('sass', function () {
 	gulp.src('src/sass/*.scss')
-	  .pipe(sass().on('error', sass.logError))
-	  .pipe(gulp.dest('src/css'));
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('src/css'));
+});
+
+gulp.task('sass:watch', function () {
+	gulp.watch('src/sass/*.scss', ['sass']);
 });
