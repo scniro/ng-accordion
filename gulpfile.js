8ca3e3aaf01d1d1eb09b5446d0156d45c2863986
Salvatore Niro <salvatore.niro@outlook.com>
2015-08-28 09:17:04 -0600

    build tasks - convert to sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,11 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
 var minifyCss = require('gulp-minify-css');
-
-gulp.task('compress', function () {
-	return gulp.src('src/js/*.js')
-		.pipe(uglify())
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(gulp.dest('dist'));
-});
+var del = require('del');
+var util = require('gulp-util');
 
 gulp.task('sass', function () {
-	gulp.src('src/sass/*.scss')
+	return gulp.src('src/sass/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
@@ -25,7 +18,7 @@ gulp.task('sass', function () {
 		.pipe(gulp.dest('src/css'));
 });
 
-gulp.task('minify-css', function () {
+gulp.task('minify-css', ['clean-css-dist'], function () {
 	return gulp.src('src/css/*.css')
 		.pipe(minifyCss())
 		.pipe(rename({
@@ -34,8 +27,31 @@ gulp.task('minify-css', function () {
 		.pipe(gulp.dest('dist'));
 });
 
+gulp.task('minify-js', ['clean-js-dist'], function () {
+	return gulp.src('src/js/*.js')
+		.pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean-css-dist', function (done) {
+
+	var file = 'dist/ng-accordian.min.css';
+	util.log(util.colors.green('cleaning'), ' -- ', util.colors.yellow(file));
+	del(file, done);
+});
+
+gulp.task('clean-js-dist', function (done) {
+
+	var file = 'dist/ng-accordian.min.js';
+	util.log(util.colors.green('cleaning'), ' -- ', util.colors.yellow(file));
+	del(file, done);
+});
+
 gulp.task('sass:watch', function () {
 	gulp.watch('src/sass/*.scss', ['sass']);
 });
 
-gulp.task('build', ['sass', 'minify-css', 'compress']);
+gulp.task('build', ['sass', 'minify-css', 'minify-js']);
