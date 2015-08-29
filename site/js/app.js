9670789451cd8376324c2ada2fb7a584b557ba81
Sal Niro <salvatore.niro@outlook.com>
2015-08-29 14:07:17 -0600

    site building

--- site/js/app.js
+++ site/js/app.js
@@ -1,31 +1,130 @@
-﻿var app = angular.module('app', ['ngAccordian']);
+﻿
+var app = angular.module('app', ['ngAccordian', 'ui.router']);
 
-app.controller('ctrl', function($scope) {
-    $scope.content = [
+app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+
+	$locationProvider.html5Mode({
+		enabled: true
+	});
+
+	$stateProvider
+		.state('home', {
+			url: '/',
+			controller: 'ctrl',
+			templateUrl: 'view/home.html'
+		})
+		.state('basic', {
+			url: 'basic',
+			controller: 'ctrl',
+			templateUrl: 'view/basic.html'
+		})
+		.state('configuration', {
+			url: 'configuration',
+			controller: 'ctrl',
+			templateUrl: 'view/configuration.html'
+		});
+});
+
+
+app.controller('ctrl', function ($scope) {
+	$scope.content = [
         { 'value': '/views/demo-content.html' },
         { 'value': '/views/demo-content.html' },
         { 'value': '/views/demo-content.html' }
-    ];
+	];
 
-    $scope.staticContent = [
+	$scope.staticContent = [
         { 'value': '<span> er </span>' },
         { 'value': '<span> um </span>' },
         { 'value': '<span> eh </span>' }
-    ];
+	];
 
-    $scope.values = [
+	$scope.values = [
         {
-            'name': 'bob',
-            'age': 20
+        	'name': 'bob',
+        	'age': 20
         }, {
-            'name': 'susy',
-            'age': 24
+        	'name': 'susy',
+        	'age': 24
         }, {
-            'name': 'daisy',
-            'age': 26
+        	'name': 'daisy',
+        	'age': 26
         }
-    ];
+	];
 
-    $scope.scopeContent = '<span>yo {{ o.name }}</span>';
+	$scope.scopeContent = '<span>yo {{ o.name }}</span>';
+
+	$scope.basic = [
+		{
+			title: 'Markup',
+			url: 'template/accordian/basic/markup.html'
+		}, {
+			title: 'JavaScript',
+			url: 'template/accordian/basic/javascript.html'
+		}
+	];
 });
+
+app.directive('tabs', ['$http', function ($http) {
+	return {
+		restrict: 'E',
+		templateUrl: 'template/tabs.html',
+		scope: {
+			tabs: '=',
+			selected: '@'
+		},
+		link: function (scope, elem, attrs) {
+
+			//scope.content = [];
+
+			//angular.forEach(scope.tabs, function (value, key) {
+
+			//	$http.get(value.url).success(function (response) {
+			//		scope.url = response;
+			//	});
+			//});
+
+			scope.currentTab = scope.tabs[scope.selected].url;
+
+
+			scope.onClickTab = function (tab) {
+				scope.currentTab = tab.url;
+			}
+
+			scope.isActiveTab = function (tabUrl) {
+				return tabUrl === scope.currentTab;
+			}
+		}
+	}
+}]);
+
+app.directive('ngHtml', [
+		'$compile', function($compile) {
+			return function(scope, elem, attrs) {
+				if (attrs.ngHtml) {
+					elem.html(scope.$eval(attrs.ngHtml));
+					$compile(elem.contents())(scope);
+				}
+				scope.$watch(attrs.ngHtml, function(newValue, oldValue) {
+					if (newValue && newValue !== oldValue) {
+						elem.html(newValue);
+						$compile(elem.contents())(scope);
+					}
+				});
+			};
+		}
+	])
+	.directive('prism', [
+		function() {
+			return {
+				restrict: 'A',
+				link: function($scope, element, attrs) {
+					element.ready(function () {
+
+						Prism.highlightElement(element[0]);
+					});
+				}
+			}
+		}
+	]);
 
