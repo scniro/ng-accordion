7a93c30454b2c66ce229a84f9d6d003369b5e187
Sal Niro <salvatore.niro@outlook.com>
2015-11-04 15:03:11 -0700

    build/cleanup

--- demo-site/js/app.js
+++ demo-site/js/app.js
@@ -1,218 +1,201 @@
 ﻿var app = angular.module('app', ['ngAccordion', 'ui.router']);
 
-app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
+app.config(['$stateProvider', '$locationProvider', function ($stateProvider, $locationProvider) {
 
-	$locationProvider.html5Mode({
-		enabled: true
-	});
+    $locationProvider.html5Mode({
+        enabled: true
+    });
 
-	$stateProvider
-		.state('home', {
-			url: '/',
-			controller: 'gettingStartedCtrl',
-			templateUrl: 'demo-site/view/getting-started.html'
-		})
-		.state('basic', {
-			url: '/examples/basic',
-			controller: 'basicCtrl',
-			templateUrl: 'demo-site/view/examples/basic.html'
-		})
-		.state('configuration', {
-			url: '/examples/configuration',
-			controller: 'configurationCtrl',
-			templateUrl: 'demo-site/view/examples/configuration.html'
-		})
-		.state('contentStrings', {
-			url: '/examples/content-strings',
-			controller: 'contentStringsCtrl',
-			templateUrl: 'demo-site/view/examples/content-strings.html'
-		})
-		.state('modelBinding', {
-			url: '/examples/model-binding',
-			controller: 'modelBindingCtrl',
-			templateUrl: 'demo-site/view/examples/model-binding.html'
-		})
-		.state('disabled', {
-			url: '/examples/disabled',
-			controller: 'disabledCtrl',
-			templateUrl: 'demo-site/view/examples/disabled.html'
-		})
-		.state('methods', {
-			url: '/methods',
-			controller: 'methodsCtrl',
-			templateUrl: 'demo-site/view/methods.html'
-		})
-		.state('callbacks', {
-			url: '/callbacks',
-			controller: 'callbacksCtrl',
-			templateUrl: 'demo-site/view/callbacks.html'
-		})
-		.state('attributes', {
-			url: '/attributes',
-			controller: 'attributesCtrl',
-			templateUrl: 'demo-site/view/attributes.html'
-		})
-		.state('icons', {
-			url: '/icons',
-			controller: 'iconsCtrl',
-			templateUrl: 'demo-site/view/icons.html'
-		});
+    $stateProvider
+        .state('home', {
+            url: '/',
+            controller: 'gettingStartedCtrl',
+            templateUrl: 'demo-site/view/getting-started.html'
+        })
+        .state('basic', {
+            url: '/examples/basic',
+            controller: 'basicCtrl',
+            templateUrl: 'demo-site/view/examples/basic.html'
+        })
+        .state('configuration', {
+            url: '/examples/configuration',
+            controller: 'configurationCtrl',
+            templateUrl: 'demo-site/view/examples/configuration.html'
+        })
+        .state('contentStrings', {
+            url: '/examples/content-strings',
+            controller: 'contentStringsCtrl',
+            templateUrl: 'demo-site/view/examples/content-strings.html'
+        })
+        .state('modelBinding', {
+            url: '/examples/model-binding',
+            controller: 'modelBindingCtrl',
+            templateUrl: 'demo-site/view/examples/model-binding.html'
+        })
+        .state('disabled', {
+            url: '/examples/disabled',
+            controller: 'disabledCtrl',
+            templateUrl: 'demo-site/view/examples/disabled.html'
+        })
+        .state('methods', {
+            url: '/methods',
+            controller: 'methodsCtrl',
+            templateUrl: 'demo-site/view/methods.html'
+        })
+        .state('callbacks', {
+            url: '/callbacks',
+            controller: 'callbacksCtrl',
+            templateUrl: 'demo-site/view/callbacks.html'
+        })
+        .state('attributes', {
+            url: '/attributes',
+            controller: 'attributesCtrl',
+            templateUrl: 'demo-site/view/attributes.html'
+        })
+        .state('icons', {
+            url: '/icons',
+            controller: 'iconsCtrl',
+            templateUrl: 'demo-site/view/icons.html'
+        });
 }]);
 
 app.controller('gettingStartedCtrl', ['$scope', function ($scope) {
-	$scope.intro = 'getting started';
+    $scope.intro = 'getting started';
 }]);
 
 app.controller('basicCtrl', ['$scope', function ($scope) {
 
-	$scope.tabs = [
-		{ 'title': 'Markup', 'url': 'demo-site/template/accordion/basic/markup.html' },
-		{ 'title': 'CSS', 'url': 'demo-site/template/accordion/basic/css.html' }
-	];
+    $scope.tabs = [
+        { 'title': 'Markup', 'url': 'demo-site/template/accordion/basic/markup.html' },
+        { 'title': 'CSS', 'url': 'demo-site/template/accordion/basic/css.html' }
+    ];
 }]);
 
 app.controller('configurationCtrl', ['$scope', function ($scope) {
 
-	$scope.tabs = [
-		{ 'title': 'Markup', 'url': 'demo-site/template/accordion/configuration/markup.html' },
-		{ 'title': 'JavaScript', 'url': 'demo-site/template/accordion/configuration/javascript.html' },
-		{ 'title': 'CSS', 'url': 'demo-site/template/accordion/configuration/css.html' }
-	];
+    $scope.tabs = [
+        { 'title': 'Markup', 'url': 'demo-site/template/accordion/configuration/markup.html' },
+        { 'title': 'JavaScript', 'url': 'demo-site/template/accordion/configuration/javascript.html' },
+        { 'title': 'CSS', 'url': 'demo-site/template/accordion/configuration/css.html' }
+    ];
 
-	$scope.content = [
-		{ 'value': 'demo-site/template/accordion/configuration/accordion-content.html' },
-		{ 'value': 'demo-site/template/accordion/configuration/accordion-content.html' },
-		{ 'value': 'demo-site/template/accordion/configuration/accordion-content.html' },
-		{ 'value': 'demo-site/template/accordion/configuration/accordion-content.html' }
-	];
+    $scope.content = [
+        { 'value': 'demo-site/template/accordion/configuration/accordion-content.html' },
+        { 'value': 'demo-site/template/accordion/configuration/accordion-content.html' },
+        { 'value': 'demo-site/template/accordion/configuration/accordion-content.html' },
+        { 'value': 'demo-site/template/accordion/configuration/accordion-content.html' }
+    ];
 }]);
 
 app.controller('contentStringsCtrl', ['$scope', function ($scope) {
 
-	$scope.tabs = [
-		{ 'title': 'Markup', 'url': 'demo-site/template/accordion/content-strings/markup.html' },
-		{ 'title': 'JavaScript', 'url': 'demo-site/template/accordion/content-strings/javascript.html' },
-		{ 'title': 'CSS', 'url': 'demo-site/template/accordion/content-strings/css.html' }
-	];
+    $scope.tabs = [
+        { 'title': 'Markup', 'url': 'demo-site/template/accordion/content-strings/markup.html' },
+        { 'title': 'JavaScript', 'url': 'demo-site/template/accordion/content-strings/javascript.html' },
+        { 'title': 'CSS', 'url': 'demo-site/template/accordion/content-strings/css.html' }
+    ];
 
-	$scope.content = [
-		{ 'value': '<p><span>hey, I\'m a string!</span></p>', 'heading': 'cool stuff' },
-		{ 'value': '<p><span>hey, I\'m a string!</span></p>', 'heading': 'cool stuff' },
-		{ 'value': '<p><span>hey, I\'m a string!</span></p>', 'heading': 'cool stuff' },
-		{ 'value': '<p><span>hey, I\'m a string!</span></p>', 'heading': 'cool stuff' },
-		{ 'value': '<p><span>hey, I\'m a string!</span></p>', 'heading': 'cool stuff' },
-		{ 'value': '<p><span>hey, I\'m a string!</span></p>', 'heading': 'cool stuff' }
-	];
+    $scope.content = [
+        { 'value': '<p><span>hey, I\'m a string!</span></p>', 'heading': 'cool stuff' },
+        { 'value': '<p><span>hey, I\'m a string!</span></p>', 'heading': 'cool stuff' },
+        { 'value': '<p><span>hey, I\'m a string!</span></p>', 'heading': 'cool stuff' },
+        { 'value': '<p><span>hey, I\'m a string!</span></p>', 'heading': 'cool stuff' },
+        { 'value': '<p><span>hey, I\'m a string!</span></p>', 'heading': 'cool stuff' },
+        { 'value': '<p><span>hey, I\'m a string!</span></p>', 'heading': 'cool stuff' }
+    ];
 }]);
 
 app.controller('modelBindingCtrl', ['$scope', function ($scope) {
 
-	$scope.tabs = [
-		{ 'title': 'Markup', 'url': 'demo-site/template/accordion/model-binding/markup.html' },
-		{ 'title': 'Template', 'url': 'demo-site/template/accordion/model-binding/model-binding-template-display.html' },
-		{ 'title': 'JavaScript', 'url': 'demo-site/template/accordion/model-binding/javascript.html' },
-		{ 'title': 'CSS', 'url': 'demo-site/template/accordion/model-binding/css.html' }
-	];
+    $scope.tabs = [
+        { 'title': 'Markup', 'url': 'demo-site/template/accordion/model-binding/markup.html' },
+        { 'title': 'Template', 'url': 'demo-site/template/accordion/model-binding/model-binding-template-display.html' },
+        { 'title': 'JavaScript', 'url': 'demo-site/template/accordion/model-binding/javascript.html' },
+        { 'title': 'CSS', 'url': 'demo-site/template/accordion/model-binding/css.html' }
+    ];
 
-	$scope.content = [
-		{ 'value': 'I\'m a model!' },
-		{ 'value': 'I\'m a model!' },
-		{ 'value': 'I\'m a model!' }
-	];
+    $scope.content = [
+        { 'value': 'I\'m a model!' },
+        { 'value': 'I\'m a model!' },
+        { 'value': 'I\'m a model!' }
+    ];
 }]);
 
 app.controller('disabledCtrl', ['$scope', function ($scope) {
 
-	$scope.tabs = [
-		{ 'title': 'Markup', 'url': 'demo-site/template/accordion/disabled/markup.html' },
-		{ 'title': 'CSS', 'url': 'demo-site/template/accordion/disabled/css.html' }
-	];
+    $scope.tabs = [
+        { 'title': 'Markup', 'url': 'demo-site/template/accordion/disabled/markup.html' },
+        { 'title': 'CSS', 'url': 'demo-site/template/accordion/disabled/css.html' }
+    ];
 }]);
 
 app.controller('methodsCtrl', ['$scope', function ($scope) {
 
-	$scope.tabs = [
-			{ 'title': 'Markup', 'url': 'demo-site/template/accordion/methods/markup.html' },
-			{ 'title': 'JavaScript', 'url': 'demo-site/template/accordion/methods/javascript.html' }
-	];
+    $scope.tabs = [
+        { 'title': 'Markup', 'url': 'demo-site/template/accordion/methods/markup.html' },
+        { 'title': 'JavaScript', 'url': 'demo-site/template/accordion/methods/javascript.html' }
+    ];
 
-	$scope.content = [
-		{ 'value': '<p><span>stuff</span></p>' },
-		{ 'value': '<p><span>stuff</span></p>' },
-		{ 'value': '<p><span>stuff</span></p>' }
-	];
-
-	$scope.$on('accordion:expand', function (e, index) {
-		console.log('accordion:expand ' + index);
-	});
-
-	$scope.$on('accordion:expand:animation', function (e, index) {
-		console.log('accordion:expand:animation ' + index);
-	});
-
-	$scope.$on('accordion:collapse', function (e, index) {
-		console.log('accordion:collapse ' + index);
-	});
-
-	$scope.$on('accordion:collapse:animation', function (e, index) {
-		console.log('accordion:collapse:collapse ' + index);
-	});
+    $scope.content = [
+        { 'value': '<p><span>stuff</span></p>' },
+        { 'value': '<p><span>stuff</span></p>' },
+        { 'value': '<p><span>stuff</span></p>' }
+    ];
 }]);
 
 app.controller('callbacksCtrl', ['$scope', '$interval', '$timeout', function ($scope, $interval, $timeout) {
 
-	var registerCallbacks = (function () {
-		var executed = false;
-		return function () {
-			if (!executed) {
-				executed = true;
+    var registerCallbacks = (function () {
+        var executed = false;
+        return function () {
+            if (!executed) {
+                executed = true;
 
-				var terminal = angular.element(document.getElementById('console'));
-				var terminalbody = angular.element(document.getElementById('console-entries'));
+                var terminal = angular.element(document.getElementById('console'));
+                var terminalbody = angular.element(document.getElementById('console-entries'));
 
-				$scope.$on('myaccordion:expand', function (e, index) {
-					terminalbody.append('<span class="console-entry"><span class="pink">myaccordion:expand</span><span> index: </span><span class="blue">' + index + '</span></span>');
-					terminal[0].scrollTop = terminal[0].scrollHeight;
-				});
+                $scope.$on('myaccordion:expand', function (e, index) {
+                    terminalbody.append('<span class="console-entry"><span class="pink">myaccordion:expand</span><span> index: </span><span class="blue">' + index + '</span></span>');
+                    terminal[0].scrollTop = terminal[0].scrollHeight;
+                });
 
-				$scope.$on('myaccordion:expand:animation', function (e, index) {
-					terminalbody.append('<span class="console-entry"><span class="pink">myaccordion:expand:animation</span><span> index: </span><span class="blue">' + index + '</span></span>');
-					terminal[0].scrollTop = terminal[0].scrollHeight;
-				});
+                $scope.$on('myaccordion:expand:animation', function (e, index) {
+                    terminalbody.append('<span class="console-entry"><span class="pink">myaccordion:expand:animation</span><span> index: </span><span class="blue">' + index + '</span></span>');
+                    terminal[0].scrollTop = terminal[0].scrollHeight;
+                });
 
-				$scope.$on('myaccordion:collapse', function (e, index) {
-					terminalbody.append('<span class="console-entry"><span class="pink">myaccordion:collpse</span><span> index: </span><span class="blue">' + index + '</span></span>');
-					terminal[0].scrollTop = terminal[0].scrollHeight;
-				});
+                $scope.$on('myaccordion:collapse', function (e, index) {
+                    terminalbody.append('<span class="console-entry"><span class="pink">myaccordion:collpse</span><span> index: </span><span class="blue">' + index + '</span></span>');
+                    terminal[0].scrollTop = terminal[0].scrollHeight;
+                });
 
-				$scope.$on('myaccordion:collapse:animation', function (e, index) {
-					terminalbody.append('<span class="console-entry"><span class="pink">myaccordion:collpse:animation</span><span> index: </span><span class="blue">' + index + '</span></span>');
-					terminal[0].scrollTop = terminal[0].scrollHeight;
-				});
+                $scope.$on('myaccordion:collapse:animation', function (e, index) {
+                    terminalbody.append('<span class="console-entry"><span class="pink">myaccordion:collpse:animation</span><span> index: </span><span class="blue">' + index + '</span></span>');
+                    terminal[0].scrollTop = terminal[0].scrollHeight;
+                });
 
-			}
-		};
-	})();
+            }
+        };
+    })();
 
-	$scope.tabs = [
-			{ 'title': 'Console', 'url': 'demo-site/template/accordion/callbacks/console.html' },
-			{ 'title': 'Markup', 'url': 'demo-site/template/accordion/callbacks/markup.html' },
-			{ 'title': 'JavaScript', 'url': 'demo-site/template/accordion/callbacks/javascript.html' }
-	];
+    $scope.tabs = [
+        { 'title': 'Console', 'url': 'demo-site/template/accordion/callbacks/console.html' },
+        { 'title': 'Markup', 'url': 'demo-site/template/accordion/callbacks/markup.html' },
+        { 'title': 'JavaScript', 'url': 'demo-site/template/accordion/callbacks/javascript.html' }
+    ];
 
-	$scope.content = [
-		{ 'value': '<p><span>stuff</span></p>' },
-		{ 'value': '<p><span>stuff</span></p>' },
-		{ 'value': '<p><span>stuff</span></p>' }
-	];
+    $scope.content = [
+        { 'value': '<p><span>stuff</span></p>' },
+        { 'value': '<p><span>stuff</span></p>' },
+        { 'value': '<p><span>stuff</span></p>' }
+    ];
 
-
-	$scope.$watch(function() {
-		return angular.element(document.getElementById('console')).html();
-	}, function (n, o) {
-		if(n && n!== o)
-			registerCallbacks();
-	});
+    $scope.$watch(function () {
+        return angular.element(document.getElementById('console')).html();
+    }, function (n, o) {
+        if (n && n !== o)
+            registerCallbacks();
+    });
 }]);
 
 app.controller('attributesCtrl', ['$scope', function ($scope) {
@@ -222,37 +205,37 @@ app.controller('iconsCtrl', ['$scope', function ($scope) {
 }]);
 
 app.directive('tabs', [function () {
-	return {
-		restrict: 'E',
-		templateUrl: 'demo-site/template/tabs.html',
-		scope: {
-			tabs: '=',
-			selected: '@'
-		},
-		link: function (scope, elem, attrs) {
+    return {
+        restrict: 'E',
+        templateUrl: 'demo-site/template/tabs.html',
+        scope: {
+            tabs: '=',
+            selected: '@'
+        },
+        link: function (scope, elem, attrs) {
 
-			if (scope.tabs) {
-				scope.currentTab = scope.tabs[scope.selected].url;
+            if (scope.tabs) {
+                scope.currentTab = scope.tabs[scope.selected].url;
 
-				scope.onClickTab = function(tab) {
-					scope.currentTab = tab.url;
-				}
+                scope.onClickTab = function (tab) {
+                    scope.currentTab = tab.url;
+                }
 
-				scope.isActiveTab = function(tabUrl) {
-					return tabUrl === scope.currentTab;
-				}
-			}
-		}
-	}
+                scope.isActiveTab = function (tabUrl) {
+                    return tabUrl === scope.currentTab;
+                }
+            }
+        }
+    }
 }]);
 
 app.directive('prism', [function () {
-	return {
-		restrict: 'A',
-		link: function (scope, elem, attrs) {
-			elem.ready(function () {
-				Prism.highlightElement(elem[0]);
-			});
-		}
-	}
+    return {
+        restrict: 'A',
+        link: function (scope, elem, attrs) {
+            elem.ready(function () {
+                Prism.highlightElement(elem[0]);
+            });
+        }
+    }
 }]);
--- gulpfile.js
+++ gulpfile.js
@@ -12,50 +12,50 @@ var rename = require('gulp-rename');
 var minifyCss = require('gulp-minify-css');
 
 gulp.task('sass', function () {
-	return gulp.src('ng-accordion/src/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('ng-accordion/src/'));
+    return gulp.src('ng-accordion/src/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('ng-accordion/src/'));
 });
 
 gulp.task('minify-css', function () {
-	return gulp.src('ng-accordion/src/*.css')
-		.pipe(minifyCss())
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(gulp.dest('ng-accordion/dist'));
+    return gulp.src('ng-accordion/src/*.css')
+        .pipe(minifyCss())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('ng-accordion/dist'));
 });
 
 gulp.task('minify-js', function () {
-	return gulp.src('ng-accordion/src/*.js')
-		.pipe(uglify())
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(gulp.dest('ng-accordion/dist'));
+    return gulp.src('ng-accordion/src/*.js')
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('ng-accordion/dist'));
 });
 
 gulp.task('sass:watch', function () {
-	gulp.watch('ng-accordion/src/*.scss', ['sass']);
+    gulp.watch('ng-accordion/src/*.scss', ['sass']);
 });
 
 // -- demo site
 gulp.task('sass-site', function () {
-	return gulp.src('demo-site/sass/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('demo-site/css'));
+    return gulp.src('demo-site/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('demo-site/css'));
 });
 
 gulp.task('sass-site:watch', function () {
-	gulp.watch('demo-site/sass/*.scss', ['sass-site']);
+    gulp.watch('demo-site/sass/*.scss', ['sass-site']);
 });
 
 gulp.task('build', ['sass', 'minify-css', 'minify-js']);
